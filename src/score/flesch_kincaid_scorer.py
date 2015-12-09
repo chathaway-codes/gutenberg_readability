2d964ab09869e2e3a74c7ab98d01f9a743f081c1
David Hedin <lucky.hedin13@gmail.com>
2015-12-08 21:12:32 -0500

    Flesch Kincaid scorer program

---
+++ src/score/flesch_kincaid_scorer.py
@@ -0,0 +1,29 @@
+from settings import FEATURE_CSV
+import csv
+
+def fkscore(words_per_sentence, syllables_per_word):
+    return (206.835 - (1.015 * words_per_sentence) - (84.6 * syllables_per_word))
+
+def fkgrade(words_per_sentence, syllables_per_word):
+    return ((0.39 * words_per_sentence) + (11.8 * syllables_per_word) - 15.59)
+
+def main():
+    with open(FEATURE_CSV, 'r') as incsv:
+        with open(FLESCH_KINCAID_CSV, 'a+') as outcsv:
+            fieldnames = ['book_id', 'flesch_kincaid_score', 'flesch_kincaid_grade']
+            writer = csv.DictWriter(outcsv, fieldnames=fieldnames)
+            writer.writeheader()
+            
+            reader = csv.DictReader(incsv)
+            for row in reader:
+                results = {}
+                results['book_id'] = row["book_id"]
+                words = float(row["average_words"])
+                syllables = float(row["average_syllables"])
+                results['flesch_kincaid_score'] = fkscore(words, syllables)
+                results['flesch_kincaid_grade'] = fkgrade(words, syllables)
+                writer.writerow(results)
+
+
+if __name__ == "__main__":
+    main()
--- src/settings.py
+++ src/settings.py
@@ -3,3 +3,4 @@ from nltk.parse import malt
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 mirror = 'file:///home/chathaway/gutenberg/www.gutenberg.lib.md.us/'
 FEATURE_CSV = os.path.join(PROJECT_ROOT, 'output.csv')
+FLESCH_KINCAID_CSV = os.path.join(PROJECT_ROOT, 'flesch_kincaid_results.csv')
